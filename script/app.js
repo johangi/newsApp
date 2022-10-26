f0dc1d8f4b92a316f523d09e1d135dbb1ecb50cd
johangi <johangiaever@gmail.com>
2022-10-26 20:04:02 +0200

    cleaning files

--- script/app.js
+++ script/app.js
@@ -1,16 +1,17 @@
-// const searchBTN = document.getElementById('searchBTN');
-// const newsSection = document.getElementById('newsSection')
+const newsSection = document.getElementById('newsSection')
 
-// const articleConst = new Article();
-
-// const loadHomepage = (data) => {
-//     const articles = data;
-    
-//     newsSection.innerHTML += `
-//     <img src=${articles.urlToImage} width='50px'></img>
-//     <h2>${articles.title}</h2>
-//     <h5>${articles.description}</h5>
-//     `
-// };
-
-// loadHomepage;
+window.onload = async function request(){
+    const result = await fetch('https://newsapi.org/v2/everything?q=Biden&apiKey=863d0a6431664782b0f4495cb5da05cb')
+    const response = await result.json();
+    const result2 = response.articles
+    for(i = 0; i < 25; i++){
+        let output = `
+        <div class='align-middle text-center mt-5'>
+        <a href="${result2[i].url}" target="_blank" class="text-dark news-article"><img src='${result2[i].urlToImage}' width='1000px' class='border rounded-4 mb-3'></img>
+        <h2>${result2[i].title}</h2></a>
+        </div>
+        `
+        newsSection.innerHTML += output;
+    }
+    return response.articles[0]
+}
