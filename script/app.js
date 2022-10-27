1ec6d0dbc8e15ad354fc530a413ef38777df5602
johangi <johangiaever@gmail.com>
2022-10-27 14:20:20 +0200

    adding more functionality and fixing search

--- script/app.js
+++ script/app.js
@@ -23,7 +23,6 @@ async function request(){
 };
 
 async function search(){
-    console.log('b');
     let query = searchInput.value;
     const fetchRequest = await fetch(`https://newsapi.org/v2/everything?q=${query}&apiKey=863d0a6431664782b0f4495cb5da05cb`);
     const searchResponse = await fetchRequest.json();
@@ -32,8 +31,8 @@ async function search(){
     for(i = 0; i < 25; i++){
         let output = `
         <div class='align-middle text-center mt-5 justify-content-center'>
-        <a href="${result[i].url}" target="_blank" class="text-dark news-article"><img src='${result[i].urlToImage}' width='70%' class='border rounded-4 mb-3'></img>
-        <h2 class="ps-5 pe-5">${result[i].title}</h2>
+        <a href="${result[i].url}" target="_blank" class="text-dark news-article"><img src='${result[i].urlToImage}' width='50%' class='border rounded-4 mb-3'></img>
+        <h2 class="paddingRightLeft">${result[i].title}</h2>
         <h5>${result[i].source.name}</h5></a>
         </div>
         `;
@@ -41,3 +40,27 @@ async function search(){
     };
     loadNewsBtn.classList.add('d-none')
 };
+
+async function categories(query){
+    loadNewsBtn.classList.add('d-none')
+    const fetchReq = await fetch(`https://newsapi.org/v2/top-headlines?q=${query}&apiKey=863d0a6431664782b0f4495cb5da05cb`);
+    const searchRes = await fetchReq.json();
+    const result = searchRes.articles;
+    newsSection.innerHTML = ``;
+    for(i = 0; i < result.length; i++){
+        let output = `
+        <div class='align-middle text-center mt-5 justify-content-center'>
+        <a href="${result[i].url}" target="_blank" class="text-dark news-article"><img src='${result[i].urlToImage}' width='50%' class='border rounded-4 mb-3'></img>
+        <h2 class="paddingRightLeft">${result[i].title}</h2>
+        <h5>${result[i].source.name}</h5></a>
+        </div>
+        `;
+        newsSection.innerHTML += output;
+    };
+}
+
+searchInput.addEventListener('keydown', e =>{
+    if(e.key === 'Enter'){
+        search();
+    }
+})
